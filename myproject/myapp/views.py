70d7dabde5ce94f14a70f6f0d5df124a12b50559
arjun2598 <arjunkumanan123@gmail.com>
2024-05-31 20:07:13 +0800

    Sample route

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+def map(request):
+    return render(request, 'map.html')
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin', admin.site.urls),
     path('', include('myapp.urls')),
+    path('map', include('myapp.urls')),
 ]
